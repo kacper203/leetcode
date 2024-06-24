162c478e7ac64cf72b263622d5825396777b6c03
Kacper Górak <kacpergleet@gmail.com>
2024-06-24 11:46:51 +0200

    FizzBuzz-v2

--- easy/412. Fizz Buzz/Fizz Buzz.cpp
+++ easy/412. Fizz Buzz/Fizz Buzz.cpp
@@ -5,17 +5,16 @@
 class Solution {
 public:
     std::vector<std::string> fizzBuzz(int n) {
-        std::vector<std::string> result(n);
-        for (int i = 1; i <= n; ++i)
-        {
+        std::vector<std::string> result;
+        for (int i = 1; i <= n; ++i){
             if(i % 3 == 0 && i % 5 == 0){
-                result[i-1] = "FizzBuzz";
+                result.push_back("FizzBuzz");
             } else if(i % 3 == 0){
-                result[i-1] = "Fizz";
+                result.push_back("Fizz");
             } else if(i % 5 == 0){
-                result[i-1] = "Buzz";
+                result.push_back("Buzz");
             } else {
-                result[i-1] = std::to_string(i);
+                result.push_back(std::to_string(i));;
             }
         }
         return result;
